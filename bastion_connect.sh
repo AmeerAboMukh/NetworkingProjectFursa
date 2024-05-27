6504771aa94bf44b3eeaea92866508d531c5317c
ameerabomukh <ameerabomukh8000@gmail.com>
2024-05-27 23:30:49 +0300

    Solution & Bastion_connect is DONE

--- bastion_connect.sh
+++ bastion_connect.sh
@@ -1,52 +1,40 @@
 #!/bin/bash
 
-# Check if the KEY_PATH environment variable is set
-if [ -z "$KEY_PATH" ]; then
+#check if the variable exists:
+if [ -z "${KEY_PATH}" ]; then
     echo "KEY_PATH env var is expected"
     exit 5
 fi
 
-# Check if at least one argument (the public instance IP) is provided
+# Check if at least public instance is provided
 if [ $# -lt 1 ]; then
     echo "Please provide bastion IP address"
     exit 5
 fi
 
 # Extract the arguments
-BASTION_IP=$1
+PUBLIC_IP=$1
 PRIVATE_IP=$2
 COMMAND=$3
 
 # Define the path to the new key file on the bastion host
-NEW_KEY_PATH_FILE="/home/ubuntu/new_key_path.pem"
+NEW_KEY_PATH_PEM="/home/ubuntu/new_key_path.pem"
 
 # Check if the new key file exists on the bastion host
-ssh -i "$KEY_PATH" ubuntu@$BASTION_IP "test -f $NEW_KEY_PATH_FILE"
+ssh -i "$KEY_PATH" ubuntu@$PUBLIC_IP "test -f $NEW_KEY_PATH_PEM"
 if [ $? -ne 0 ]; then
     echo "Failed to find the new key path file on the bastion host"
     exit 5
 fi
 
-# Function to connect to the public instance
-connect_public() {
-    ssh -t -i "$KEY_PATH" ubuntu@"$BASTION_IP"
-}
-
-# Function to connect to the private instance via the public instance
-connect_private() {
-    ssh -t -i "$KEY_PATH" ubuntu@"$BASTION_IP" "ssh -t -i $NEW_KEY_PATH_FILE ubuntu@$PRIVATE_IP"
-}
-
-# Function to run a command on the private instance via the public instance
-run_command_private() {
-    ssh -t -i "$KEY_PATH" ubuntu@"$BASTION_IP" "ssh -t -i $NEW_KEY_PATH_FILE ubuntu@$PRIVATE_IP '$COMMAND'"
-}
-
-# Determine which operation to perform
+# Choose one of the 3 cases to run :
+# 1. Run a command in the private machine
+# 2. Connect to the private instance from your local machine
+# 3. Connect to the public instance
 if [ -n "$PRIVATE_IP" ] && [ -n "$COMMAND" ]; then
-    run_command_private
+        ssh -t -i "$KEY_PATH" ubuntu@"$PUBLIC_IP" "ssh -t -i $NEW_KEY_PATH_FILE ubuntu@$PRIVATE_IP '$COMMAND'"
 elif [ -n "$PRIVATE_IP" ]; then
-    connect_private
+        ssh -t -i "$KEY_PATH" ubuntu@"$PUBLIC_IP" "ssh -t -i $NEW_KEY_PATH_PEM ubuntu@$PRIVATE_IP"
 else
-    connect_public
+        ssh -t -i "$KEY_PATH" ubuntu@"$PUBLIC_IP"
 fi
